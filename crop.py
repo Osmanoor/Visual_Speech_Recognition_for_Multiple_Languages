8c107816ee4fffa3ff3c263b77f11f145f8bc25e
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-05-25 22:27:03 +0200

    Add files via upload

---
+++ crop.py
@@ -0,0 +1,18 @@
+import cv2
+import torchvision
+from pipelines.data.data_module import AVSRDataLoader
+from pipelines.detectors.mediapipe.detector import LandmarksDetector
+
+def save2vid(filename, vid, frames_per_second):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    torchvision.io.write_video(filename, vid, frames_per_second)
+
+def preprocess_video(src_filename, dst_filename):
+    landmarks = landmarks_detector(src_filename)
+    data = dataloader.load_data(src_filename, landmarks)
+    fps = cv2.VideoCapture(src_filename).get(cv2.CAP_PROP_FPS)
+    save2vid(dst_filename, data, fps)
+    return
+
+dataloader = AVSRDataLoader(modality="video", speed_rate=1, transform=False, detector="mediapipe", convert_gray=False)
+landmarks_detector = LandmarksDetector()
---
+++ stapp.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tempfile
+import pipeline
+import crop
+
+# Set the title of the app
+st.title("Visual Speech Recognition")
+
+# Add a header
+st.header("Upload your MP4 video file")
+
+# Create a file uploader
+uploaded_file = st.file_uploader("Choose an MP4 video", type=["mp4"])
+
+if uploaded_file is not None:
+    # Display the uploaded video
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(uploaded_file.read())
+        video_file = temp_file.file
+else:
+    video_file = "download.mp4"
+st.video(video_file)
+
+#Predict The Text
+result = pipeline.VSR(temp_file.name)
+st.text("You said: " + result)
+
+#Crop video
+crop_video = "crop_video.mp4"
+crop.preprocess_video(video_file,crop_video)
+st.video(crop_video)
+
+
